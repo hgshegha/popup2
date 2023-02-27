e8c2236e0006b643e4850381ac4becc1c15ef398
hgshegha <73647697+hgshegha@users.noreply.github.com>
2023-02-27 14:13:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
 
 
 
- import myJson from './jobs.json' assert {type: 'json'};
 
 
 
 
-myJson.response.results.forEach((item) =>
-{
-    let block = document.querySelector('.message-wrapper');
- 
-    block.innerHTML += `
-            <div class="message">
-                <div class="img"><img src="/https___s3.amazonaws.com_appforest_uf_f1669301075946x302729185013369340_al-pacino-scarface.jpg" alt=""></div>
-                <div class="text-main">
-                    <span id="text1" class="text">${item.category1_text}</span>
-                    <span id="text2" class="text">${item.jobtype1_list_text}</span>
-                    <span id="text3" class="text">${item.location_geographic_address.address}</span>
-                </div>
 
-                <div class="lorem-text">
-                    <span>lorem ipsum just stupit text bruh</span>
-                </div>
-
-                <div class="btns">
-                    <button id="first-btn">Mehr Infos</button>
-                    <button id="second-btn">WhatsApp Bewerbung⚡</button>
-                </div>
-          </div>
-          <hr>
-    `;
-    
-    document.body.append(block);
-});
 
 let test = document.querySelector('.message-wrapper')
 let body = document.querySelector('body')
